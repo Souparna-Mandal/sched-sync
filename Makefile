2fb682ee6c6e3912df4a4bb67d67b5f88d062e32
Souparna Mandal <souparna2003@gmail.com>
2025-01-28 04:27:45 +0000

    added code demonstrating the scheduler subversion problem

---
+++ Makefile
@@ -0,0 +1,63 @@
+CYCLE_PER_US = 1800L
+
+ifndef CYCLE_PER_US
+$(error CYCLE_PER_US not set. Set CYCLE_PER_US according to your machine CPU-SPEED)
+endif
+
+CC = gcc
+CFLAGS = -Wall -g -DCYCLE_PER_US=$(CYCLE_PER_US)
+
+# Include directory where fiber.h is located
+INCLUDE_DIR = /home/souparna/diss/sched-sync/libfiber/include
+
+# Directory where the libfiber library is located
+LIB_DIR = /home/souparna/diss/sched-sync/libfiber
+
+export LD_LIBRARY_PATH +=$(LIB_DIR)
+
+SRC_DIR = src
+OBJ_DIR = obj
+BIN_DIR = bin
+INCLUDE_DIR_LOCAL = ./include
+
+# Source files and object files
+SRCS = $(wildcard $(SRC_DIR)/*.c)
+OBJS = $(SRCS:$(SRC_DIR)/%.c=$(OBJ_DIR)/%.o)
+TARGET = $(BIN_DIR)/subversion_demo
+
+# Add -pthread flag for threading support and include directory
+CFLAGS += -I$(INCLUDE_DIR) -I$(INCLUDE_DIR_LOCAL) -pthread
+
+# Library flags
+LDFLAGS = -L$(LIB_DIR) -lfiber
+
+# Rule for the target executable
+$(TARGET): $(OBJS) | $(BIN_DIR)
+	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS)
+
+# Rule for creating object files
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c | $(OBJ_DIR)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+# Create the object directory if it doesn't exist
+$(OBJ_DIR):
+	mkdir -p $(OBJ_DIR)
+
+# Create the binary directory if it doesn't exist
+$(BIN_DIR):
+	mkdir -p $(BIN_DIR)
+
+# Clean up object files, binary files, and directories
+clean:
+	rm -rf $(OBJ_DIR)/*.o $(BIN_DIR)/*
+
+# Target for building with FAIRLOCK
+fairlock: CFLAGS += -DFAIRLOCK
+fairlock: $(TARGET)
+
+# Target for building with MUTEX
+mutex: CFLAGS += -DMUTEX
+mutex: $(TARGET)
+
+
+.PHONY: clean fairlock mutex
---
+++ src/problem.c
@@ -0,0 +1,135 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <time.h>
+
+#include "fiber_manager.h"
+// #include "test_helper.h"
+
+typedef unsigned long long ull;
+typedef struct timespec timespec_t;
+
+typedef struct {
+    int id;
+    ull cs;
+    ull num_lock_acquired;
+    ull loop_count_in_cs;
+    ull lock_hold_time;
+    struct timeval start_time;
+    ull duration;
+} task_t;
+
+fiber_mutex_t mutex;
+
+ull time_diff(struct timeval t0, struct timeval t1) {
+    return (ull)((t1.tv_sec - t0.tv_sec) * 1000000 + (t1.tv_usec - t0.tv_usec));
+}
+
+void* run_func(void* param) {
+    task_t *task = (task_t *)param;
+
+    struct timeval now, start;
+    ull lock_acquires = 0;
+    ull lock_hold = 0;
+    ull loop_in_cs = 0;
+
+    gettimeofday(&now, NULL);
+
+    while (time_diff(task->start_time, now) < task->duration * 1000000) {
+#ifdef FAIRLOCK
+        // TODO: Implement FAIRLOCK logic here
+#endif
+#ifdef MUTEX
+        fiber_mutex_lock(&mutex);
+#endif
+
+        gettimeofday(&start, NULL);
+        lock_acquires++;
+
+        do {
+            loop_in_cs++;
+            gettimeofday(&now, NULL);
+        } while (time_diff(start, now) < task->cs);
+
+        lock_hold += time_diff(start, now);
+
+#ifdef FAIRLOCK
+        // TODO: Implement FAIRLOCK logic here
+#endif
+#ifdef MUTEX
+        fiber_mutex_unlock(&mutex);
+#endif
+
+        gettimeofday(&now, NULL);
+    }
+
+    task->num_lock_acquired = lock_acquires;
+    task->loop_count_in_cs = loop_in_cs;
+    task->lock_hold_time = lock_hold;
+
+    printf("id %02d "
+           "loop %10llu "
+           "lock_acquires %8llu "
+           "lock_hold(us) %10llu\n",
+           task->id,
+           task->loop_count_in_cs,
+           task->num_lock_acquired,
+           task->lock_hold_time);
+
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("usage: %s <nthreads> <duration> <critical section 1> <critical section 2> ...\n", argv[0]);
+        printf("nthreads - number of threads\n");
+        printf("duration - duration of the experiment in seconds (s)\n");
+        printf("critical section - critical section size in microseconds (us)\n");
+        return 1;
+    }
+
+    int nthreads = atoi(argv[1]);
+    ull duration = atoll(argv[2]);
+
+    fiber_manager_init(nthreads + 1);
+    task_t tasks[nthreads];
+    fiber_t* fibers[nthreads];
+
+    struct timeval start_time;
+    gettimeofday(&start_time, NULL);
+
+    for (int i = 0; i < nthreads; i++) {
+        tasks[i].id = i;
+        tasks[i].cs = atoll(argv[3 + i]);
+        tasks[i].num_lock_acquired = 0;
+        tasks[i].loop_count_in_cs = 0;
+        tasks[i].lock_hold_time = 0;
+        tasks[i].start_time = start_time;
+        tasks[i].duration = duration;
+    }
+
+#ifdef FAIRLOCK
+    // TODO: Initialize FAIRLOCK here
+#endif
+#ifdef MUTEX
+    fiber_mutex_init(&mutex);
+#endif
+
+    for (int i = 0; i < nthreads; i++) {
+        fibers[i] = fiber_create(10240, &run_func, (void*)&tasks[i]);
+    }
+
+    for (int i = 0; i < nthreads; i++) {
+        fiber_join(fibers[i], NULL);
+    }
+
+#ifdef MUTEX
+    fiber_mutex_destroy(&mutex);
+#endif
+
+    // fiber_manager_print_stats();
+    fiber_shutdown();
+
+    return 0;
+}
